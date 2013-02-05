a1e39c6e1910ade27035c0d6a1df15c5abf807e7
Kris <krismolendyke+github@gmail.com>
2013-02-05 15:52:02 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Adventures In Window Management
+
+With [Slate](https://github.com/jigish/slate) and its fun [JavaScript configuration](https://github.com/jigish/slate/wiki/JavaScript-Configs).
---
+++ slate.js
@@ -0,0 +1,73 @@
+// Use a constant "mash" keyboard chord as modal switch.
+var MODAL = ':ctrl,alt,cmd,space';
+
+
+// Configure globals.
+S.configAll({
+    'keyboardLayout': 'dvorak'
+});
+
+
+// Quick relaunching during trial and error phase.
+S.bind('r' + MODAL, S.op('relaunch'));
+
+
+// Generally, I do not want Slate to mess with Emacs.
+var ignoreEmacs = function(win, f) {
+    if (!/emacs/i.test(win.app().name())) {
+        f.apply(this, arguments);
+    }
+};
+
+
+/*
+ * Push bindings.
+ */
+S.bind('right' + MODAL, function(win) {
+    ignoreEmacs(win, function() {
+        win.doOperation(S.op('push', {
+            'direction': 'right',
+            'style': 'bar-resize:screenSizeX/1.7'
+        }));
+    });
+});
+
+
+S.bind('left' + MODAL, function(win) {
+    ignoreEmacs(win, function() {
+        win.doOperation(S.op('push', {
+            'direction': 'left',
+            'style': 'bar-resize:screenSizeX/2.5'
+        }));
+    });
+});
+
+
+/*
+ * Window hints, Dvorak-style.
+ */
+S.bind('h' + MODAL, function(win) {
+    win.doOperation(S.op('hint', { 'characters': 'aoeusnthid' }));
+});
+
+
+/*
+ * Grid settings for my laptop and 1080p monitors.
+ */
+S.bind('g' + MODAL, function(win) {
+    ignoreEmacs(win, function() {
+        win.doOperation(S.op('grid', {
+            'grids': {
+                '1366x768': {
+                    'width': 6,
+                    'height': 3
+                },
+                '1920x1080': {
+                    'width': 8,
+                    'height': 5
+                }
+            },
+            'padding': 5
+        }));
+    });
+});
